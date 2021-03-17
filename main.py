05d29a2c2d3d40356e17e689e5721bc039d76820
Gabriel Dias Gonçalves <gabrieldiadeath90@gmail.com>
2021-03-17 20:36:20 -0300

    Adicionando diretório primitivos

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello World')
+import primitivos.inteiros as exemplo_inteiros
+import primitivos.operacoes as exemplo_operacoes
+#Fundamentos de lógica de programação com Python
+
+alternativa = 2
+
+if alternativa == 1:
+    exemplo_inteiros.mostrar_exemplo()
+elif alternativa == 2:
+    exemplo_operacoes.mostrar_exemplo()
---
+++ primitivos/operacoes.py
@@ -0,0 +1,10 @@
+# Operações
+
+def mostrar_exemplo():
+    print(2 + 2)   # Soma
+    print(2 - 2)   # Subtração
+    print(2 * 2)   # Multiplicação
+    print(2 / 2)   # Divisão
+    print(2 // 2)  # Divisão inteira
+    print(2 % 2)   # Resto da divisão
+    print(2 ** 2)  # Potência
