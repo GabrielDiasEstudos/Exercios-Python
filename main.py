cca4b512e52ef6de69adbadb77d4935b9ae311ab
Gabriel Dias Gonçalves <gabrieldiadeath90@gmail.com>
2021-03-17 21:38:24 -0300

    Adicionando exemplo de strings

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import primitivos.inteiros as exemplo_inteiros
 import primitivos.ponto_flutuantes as ex_pontoflutuante
+import primitivos.textos as ex_texto
 
 import operadores.aritmeticos as exemplo_operacoes
 
 #Fundamentos de lógica de programação com Python
 
-alternativa = 3
+alternativa = 4
 
 if alternativa == 1:
     exemplo_inteiros.mostrar_exemplo()
@@ -13,3 +14,5 @@ elif alternativa == 2:
     exemplo_operacoes.mostrar_exemplo()
 elif alternativa == 3:
     ex_pontoflutuante.mostrar_exemplo()
+elif alternativa == 4:
+    ex_texto.mostrar_exemplo()
---
+++ primitivos/textos.py
@@ -0,0 +1,9 @@
+# Cadeia de caractéres (String)
+
+def mostrar_exemplo():
+    print('Hello World')
+
+    print(type('Olá'))
+
+    nome = "Renzo's name \" \nPulando linha"
+    print(nome)
