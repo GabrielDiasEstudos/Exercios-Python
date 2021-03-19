c0f71df55d99c50da820192839767a5cccfe6c27
Gabriel Dias Gonçalves <gabrieldiadeath90@gmail.com>
2021-03-18 21:49:55 -0300

    Aprendendo funções de listas

--- sequencias/funcoes_listas.py
+++ sequencias/funcoes_listas.py
@@ -1,4 +1,23 @@
 # Adição e Remoção
 
 def mostrar_exemplo():
-    print('Hello World')
+    numeros = list(range(1, 11))
+
+    # print(dir(numeros)) # lista as funções da lista
+
+    print(numeros)
+    numeros.pop()  # remove o último item
+    # help(numeros.pop())  # função documentação
+
+    print(numeros)
+
+    numeros.append(10) # adiciona um elemento
+
+    print(numeros)
+
+    numeros.extend([11, 12]) # junta uma lista com a outra
+    print(numeros)
+
+    print([1, 3] + [2, 4])
+
+    print([1, 3] * 3)
